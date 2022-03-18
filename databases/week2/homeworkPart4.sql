76c2f1d37728578d30baa26404597303b6cc2faf
Tatjana Ocedova <tatjanaocedova@gmail.com>
2022-03-18 11:40:07 +0100

    Add update part 4

--- databases/week2/homeworkPart4.sql
+++ databases/week2/homeworkPart4.sql
@@ -7,11 +7,11 @@ USE library;
 -- Create data table
 CREATE TABLE `member` (
     `member_id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     `first_name` varchar(255) NOT NULL,
-     `lastname` varchar(255) NOT NULL,
-     `email` varchar(255) NOT NULL,
-     `phone` int(10) NOT NULL
-     );
+    `first_name` varchar(255) NOT NULL,
+    `lastname` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `phone` int(10) NOT NULL
+    );
 
 CREATE TABLE `book` (
     `book_id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -22,9 +22,26 @@ CREATE TABLE `book` (
     );
 
 CREATE TABLE `borrowing_log` (
-    `book_id` int(10) unsigned,
-    `name` varchar(255) NOT NULL,
-    `author` varchar(255) NOT NULL,
-    `publish_date` int(10) NOT NULL,
-    `numbers_of_copy` int(10) NOT NULL
+    `book_id` int(10) unsigned NOT NULL,
+    `member_id` int(10) unsigned NOT NULL,
+    `borrowing_date` DATETIME NOT NULL
     );
+
+ALTER TABLE book
+DROP COLUMN publish_date;
+
+ALTER TABLE book
+ADD publish_date DATETIME NOT NULL;
+
+SELECT
+    *
+FROM
+    book;
+
+
+-- Forgot to add foreing keys
+ALTER TABLE borrowing_log
+ADD  CONSTRAINT `fk_book` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE borrowing_log
+ADD  CONSTRAINT `fk_member` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE;
