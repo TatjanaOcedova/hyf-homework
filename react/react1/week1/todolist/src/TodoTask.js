61d43f0ffda21a45a80dd7974fbefc94c7046f41
Tatjana Ocedova <tatjanaocedova@gmail.com>
2022-05-10 11:14:34 +0200

    Add files

--- react/react1/week1/todolist/src/TodoTask.js
+++
@@ -1,7 +0,0 @@
-const tasks = [
-  { description: "Get out of bed", deadline: "Wed Sep 13 2017" },
-  { description: "Brush teeth", deadline: "Thu Sep 14 2017" },
-  { description: "Eat breakfast", deadline: "Fri Sep 15 2017" },
-];
-
-export default tasks;
--- react/react1/week1/todolist/src/TodoTaskList.js
+++
@@ -1,9 +0,0 @@
-function TodoTaskList(props) {
-  return (
-    <div>
-      <span>{props.description}</span>, <span>{props.deadline}</span>
-    </div>
-  );
-}
-
-export default TodoTaskList;
